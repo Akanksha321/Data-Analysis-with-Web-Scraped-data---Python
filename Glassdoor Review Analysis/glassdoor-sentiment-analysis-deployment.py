5dba8dd5090b6c3c1314b6ab6cca7f9c2b701d4b
Akanksha321 <akankshanagar16@gmail.com>
2021-01-04 14:21:21 +0530

    Add files via upload

--- Glassdoor Review Analysis/glassdoor-sentiment-analysis-deployment.py
+++ Glassdoor Review Analysis/glassdoor-sentiment-analysis-deployment.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
+# In[98]:
 
 
 import nltk
 nltk.download('averaged_perceptron_tagger')
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report,confusion_matrix
 
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-# Gensim
-import gensim
-import gensim.corpora as corpora
-from gensim.utils import simple_preprocess
-from gensim.models import CoherenceModel
-
+# nltk
+import string
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.tokenize import WhitespaceTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn import decomposition
 from nltk.stem.porter import PorterStemmer
 from sklearn.model_selection import train_test_split
 
@@ -30,15 +32,14 @@ import spacy
 
 import matplotlib.pyplot as plt
 
-reviews = pd.read_csv('principal_data.csv')
+reviews = pd.read_csv('employee_reviews.csv')
 
 
-# In[28]:
+# In[99]:
 
 
 # converting all the columns name to lower case for easy access
 reviews.columns  = reviews.columns.str.lower()
-from nltk.corpus import wordnet
 
 def get_wordnet_pos(pos_tag):
     if pos_tag.startswith('J'):
@@ -52,12 +53,11 @@ def get_wordnet_pos(pos_tag):
     else:
         return wordnet.NOUN
     
-import string
-from nltk import pos_tag
-from nltk.corpus import stopwords
-from nltk.tokenize import WhitespaceTokenizer
-from nltk.stem import WordNetLemmatizer
 
+# remove stop words
+stop_words = stopwords.words('english')
+not_stopwords = {'no', 'not', 'but', 'all', 'think', 'of'}
+final_stop_words = set([word for word in stop_words if word not in not_stopwords])
 def clean_text(text):
     # lower text
     text = text.lower()
@@ -65,10 +65,7 @@ def clean_text(text):
     text = [word.strip(string.punctuation) for word in text.split(" ")]
     # remove words that contain numbers
     text = [word for word in text if not any(c.isdigit() for c in word)]
-    # remove stop words
-    stop_words = stopwords.words('english')
-    not_stopwords = {'no', 'not', 'but', 'all', 'think', 'of'}
-    final_stop_words = set([word for word in stop_words if word not in not_stopwords])
+    
     text = [x for x in text if x not in final_stop_words]
     # remove empty tokens
     text = [t for t in text if len(t) > 0]
@@ -90,18 +87,18 @@ reviews['pros'] = [x.replace("\r\n","") for x in reviews['pros']]
 
 reviews['cons'] = [x.replace("\r\n","") for x in reviews['cons']]
 reviews['feedback'] = reviews['pros'] + " " + reviews['cons']
-reviews['sentiment']=reviews['overall_rating'].apply(lambda x: 2 if int(x)>3 else (1 if int(x) ==3 else 0))
-reviews.drop(['pros','location','review_date', 'cons', 'author_active', 'overall_rating', 'review_title', 'outlook', 'recommends', 'review_text'], axis = 1, inplace = True)
+reviews['sentiment']=reviews['overall-ratings'].apply(lambda x: 2 if float(x)>3 else (1 if float(x) ==3 else 0))
+reviews.drop(['pros','unnamed: 0','work-balance-stars','culture-values-stars', 'cons', 'career-opportunities-stars', 'overall-ratings', 'comp-benefit-stars', 'senior-management-stars'], axis = 1, inplace = True)
 
 
-# In[29]:
+# In[100]:
 
 
 
 
 # add tf-idfs columns
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf = TfidfVectorizer(min_df = 10)
+tfidf = TfidfVectorizer(min_df = 10,ngram_range=(1, 3), stop_words=final_stop_words)
 tfidf_result = tfidf.fit_transform(reviews["feedback"]).toarray()
 tfidf_df = pd.DataFrame(tfidf_result, columns = tfidf.get_feature_names())
 tfidf_df.columns = ["word_" + str(x) for x in tfidf_df.columns]
@@ -109,24 +106,18 @@ tfidf_df.index = reviews.index
 reviews = pd.concat([reviews, tfidf_df], axis=1)
 
 
-# In[30]:
-
-
-reviews.shape
-
-
-# In[31]:
+# In[101]:
 
 
 import pickle
 pickle.dump(tfidf, open('transform.pkl', 'wb'))
 
 
-# In[32]:
+# In[102]:
 
 
 label = "sentiment"
-ignore_cols = [label, "feedback"]
+ignore_cols = [label, "feedback", "company"]
 features = [c for c in reviews.columns if c not in ignore_cols]
 X_train, X_test, y_train, y_test = train_test_split(reviews[features], reviews[label], test_size = 0.20, random_state = 42)
 from sklearn.ensemble import RandomForestClassifier
@@ -138,9 +129,19 @@ rf.fit(X_train, y_train)
 feature_importances_df = pd.DataFrame({"feature": features, "importance": rf.feature_importances_}).sort_values("importance", ascending = False)
 feature_importances_df.head(20)
 
+
 rf.score(X_test, y_test)
 filename = 'senti_model.pkl'
 pickle.dump(rf,open(filename, 'wb'))
+
+
+# In[103]:
+
+
+predicted= rf.predict(X_test)
+
+print('Accuracy: \n', accuracy_score(y_test, predicted))
+print('Classification Report: \n', classification_report(y_test, predicted))
 
 
 # In[ ]:
